b598302f4e5d12fe7dbffb792d4855a7443d5c3f
Eason Lu <github@easonlu.ml>
2022-10-13 15:03:06 -0700

    init

---
+++ passward.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <string>
+#include <locale>
+
+using namespace std;
+
+int
+main ()
+{
+  //init var(s).
+  string name;
+  string webside;
+  int passwardlength;
+  string capitalized;
+  int capitalized_int;
+  string special;
+  int special_int;
+  string reversable;
+  int reversable_int;
+  //question
+    cout << "Passward:>Welcome to the passward generater." << endl;
+  cout << "Passward:>Please type your name:";
+  cin >> name;
+  cout << endl <<
+    "Passward:>Please type the webside you are going to craete a passward for:";
+  cin >> webside;
+  cout << endl << "Passward:>Please type how long you want your passward:";
+  cin >> passwardlength;
+  while (true)
+    {
+      cout << endl <<
+	"Passward:>Do you want to use CAPITALIZED letter? [yes/no]:";
+      cin >> capitalized;
+      cout << endl;
+      if (capitalized == "yes")
+	{
+	  capitalized_int = 1;
+	  cout << endl << "Imput accepted as yes. " << "Your imput is:" <<
+	    capitalized;
+	  break;
+	}
+      //end if
+      else if (capitalized == "no")
+	{
+	  capitalized_int = 0;
+	  cout << endl << "Imput accepted as no. " << "Your imput is:" <<
+	    capitalized;
+	  break;
+	}
+      //end else if
+      else
+	{
+	  cout << "Error:invalid input please try again.";
+	}
+      //end else
+    }
+  //end while
+  while (true)
+    {
+      cout << endl << "Passward:>Do you want special letters? [yes/no]:";
+      cin >> special;
+      if (special == "yes")
+	{
+	  special_int = 1;
+	  cout << endl << "Imput accepted as yes. " << "Your imput is:" <<
+	    special;
+	  break;
+	}
+      //end if
+      else if (special == "no")
+	{
+	  special_int = 0;
+	  cout << endl << "Imput accepted as no. " << "Your imput is:" <<
+	    special;
+	  break;
+	}
+      //end else if
+      else
+	{
+	  cout << "Error:invalid input please try again.";
+	}
+    }
+  //end else
+  //end while
+  while (true)
+    {
+      cout << endl << "Do you want reversable algeoism?" << endl <<
+	"Warning:Choosing yes will ignore all settings." << endl <<
+	"Yor input[yes/no]:";
+      cin >> reversable;
+      if (reversable == "yes")
+	{
+	  reversable_int = 1;
+	  cout << endl << "Imput accepted as yes. " << "Your imput is:" <<
+	    reversable;
+	  break;
+	}
+      //end if
+      else if (reversable == "no")
+	{
+	  reversable_int = 0;
+	  cout << endl << "Imput accepted as no. " << "Your imput is:" <<
+	    reversable;
+	  break;
+	}
+      //end else if
+      else
+	{
+	  cout << "Error:invalid input please try again.";
+	}
+    }
+  cout << endl << "Start genrating......";
+  if (reversable_int == 1)
+    {
+      string procress_string;
+      procress_string = name + "-" + webside;
+      cout << endl << "debug:" << procress_string;
+
+    }
+  else
+    {
+      if (special_int == 1)
+	{
+	  if (capitalized_int == 1)
+	    {
+	      //tt
+	    }
+	  else
+	    {
+	      //tf
+	    }
+	}
+      else
+	{
+	  if (capitalized_int == 1)
+	    {
+	      //ft
+	    }
+	  else
+	    {
+	      //ff
+	    }
+	}
+    }
+  return 0;
+}
