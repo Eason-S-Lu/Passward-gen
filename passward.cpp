425bb83f916846d30bed3463b0fb3de1c22098b6
Eason Lu <ghcodesign@outlook.com>
2023-09-12 15:22:50 -0700

    geenral improvment to base64

--- passward.cpp
+++ passward.cpp
@@ -6,64 +6,89 @@
 #include <algorithm>
 #include <stdexcept>
 #include <random>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 using namespace std;
-#define SIZE 1000
+
+char *safeStrcat(const char *s1, const char *s2)
+{
+    char *result = (char *)malloc(strlen(s1) + strlen(s2) + 1);
+    if (result == NULL)
+    {
+        return NULL;
+    }
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
 
 // Takes string to be encoded as input
 // and its length and returns encoded string
-char *base64Encoder(char input_str[], int len_str)
+static const char char_set[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+char *base64Encoder(const char input_str[], int len_str)
 {
-    // Character set of base64 encoding scheme
-    char char_set[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
     // Resultant string
-    char *res_str = (char *)malloc(SIZE * sizeof(char));
+    char *res_str = (char *)malloc(((len_str * 4 / 3) + 4) * sizeof(char));
+    if (res_str == NULL)
+    {
+        // Memory allocation failed
+        return NULL;
+    }
+
     int index, no_of_bits = 0, padding = 0, val = 0, count = 0, temp;
     int i, j, k = 0;
-    // Loop takes 3 characters at a time from
-    // input_str and stores it in val
+
+    // Loop takes 3 characters at a time from input_str and stores it in val
     for (i = 0; i < len_str; i += 3)
     {
         val = 0, count = 0, no_of_bits = 0;
+
         for (j = i; j < len_str && j <= i + 2; j++)
         {
-            // binary data of input_str is stored in val
+            // Binary data of input_str is stored in val
             val = val << 8;
             // (A + 0 = A) stores character in val
             val = val | input_str[j];
-            // calculates how many time loop
-            // ran if "MEN" -> 3 otherwise "ON" -> 2
+            // Calculates how many times the loop ran
             count++;
         }
+
         no_of_bits = count * 8;
-        // calculates how many "=" to append after res_str.
+
+        // Calculates how many "=" to append after res_str
         padding = no_of_bits % 3;
-        // extracts all bits from val (6 at a time)
-        // and find the value of each block
+
+        // Extracts all bits from val (6 at a time) and finds the value of each block
         while (no_of_bits != 0)
         {
-            // retrieve the value of each block
+            // Retrieve the value of each block
             if (no_of_bits >= 6)
             {
                 temp = no_of_bits - 6;
-                // binary of 63 is (111111) f
+                // Binary of 63 is (111111) f
                 index = (val >> temp) & 63;
                 no_of_bits -= 6;
             }
             else
             {
                 temp = 6 - no_of_bits;
-                // append zeros to right if bits are less than 6
+                // Append zeros to the right if bits are less than 6
                 index = (val << temp) & 63;
                 no_of_bits = 0;
             }
             res_str[k++] = char_set[index];
         }
     }
-    // padding is done here
+
+    // Padding is done here
     for (i = 1; i <= padding; i++)
     {
         res_str[k++] = '=';
     }
+
     res_str[k] = '\0';
     return res_str;
 }
@@ -188,14 +213,37 @@ int main()
          << "Passward:>Start genrating......" << endl;
     if (reversable_int == 1)
     {
-        char input_str[] = "MENON";
-        int len_str;
-        // calculates length of string
-        len_str = sizeof(input_str) / sizeof(input_str[0]);
-        // to exclude '\0' character
-        len_str -= 1;
-        cout << "Input string is : " << input_str << endl;
-        cout << "Encoded string is : " << base64Encoder(input_str, len_str) << endl;
+        const char *sname = name.c_str();
+        const char *surl = webside.c_str();
+        char *combined_str = safeStrcat(sname, "@");
+        if (combined_str != NULL)
+        {
+            // Append the URL to the combined string
+            combined_str = safeStrcat(combined_str, surl);
+
+            // Call the base64Encoder function
+            char *encoded_str = base64Encoder(combined_str, strlen(combined_str));
+
+            if (encoded_str != NULL)
+            {
+                // Print the encoded string
+                printf("Encoded string: %s\n", encoded_str);
+
+                // Free memory
+                free(encoded_str);
+            }
+            else
+            {
+                printf("Memory allocation failed for encoded string.\n");
+            }
+
+            // Free memory for the combined string
+            free(combined_str);
+        }
+        else
+        {
+            printf("Memory allocation failed for combined string.\n");
+        }
     }
     else
     {
